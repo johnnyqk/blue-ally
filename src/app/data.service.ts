6688f3680106acc089cf0d518c4297f4d5bcfbbf
johnny <johnny.tmg@gmail.com>
2018-10-23 10:53:03 -0500

    Adding notes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,17 +5,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
-
+// ***All below urls need to be changed based on where your API is hosting.
   constructor(private http: HttpClient) { }
-
-  getSubnets() {
+  // Get all Subnets (Parent Level)
+   getSubnets() {
     return this.http.get('http://localhost:8080/allsubnets')
   }
   
+  // Get All IPs by cidr
   getSubnetIp(cidr) {
    return this.http.get('http://localhost:8080/subnet/'+cidr)
   }
 
+  // Get Subnet/IP detail by ip Id
   getSubnetDetail(id) {
     return this.http.get('http://localhost:8080/subnetdetail/'+id)
   }
